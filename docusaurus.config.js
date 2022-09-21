dc2ec75b2236d3a69ab5ec566b0c8b5538e68e08
Jonas Tranberg <jonastranberg93@gmail.com>
2022-09-21 22:04:34 +0200

    Change to docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,84 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: "opsway",
+    tagline: "Real-time monitoring and alerting for your infrastructure",
+    url: "https://opsway.io",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+
+    organizationName: "opsway", // Usually your GitHub org/user name.
+    projectName: "docs", // Usually your repo name.
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                },
+                blog: {
+                    showReadingTime: true,
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            navbar: {
+                title: "opsway",
+                logo: {
+                    alt: "My Site Logo",
+                    src: "https://avatars.githubusercontent.com/u/104105414",
+                    href: "/",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "overview",
+                        position: "left",
+                        label: "Docs",
+                    },
+                    { to: "/docs/api", label: "API", position: "left" },
+
+                    { to: "/blog", label: "Blog", position: "left" },
+                    {
+                        href: "https://github.com/opsway-io/docs",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+            colorMode: {
+                defaultMode: "dark",
+                disableSwitch: true,
+            },
+        }),
+};
+
+module.exports = config;
