d1f196dc01122fef2c0da069e990749a76945243
Jonas Tranberg <jonastranberg93@gmail.com>
2022-10-08 01:41:47 +0200

    remove trailing slash in paths

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
     favicon: "img/favicon.ico",
     organizationName: "opsway", // Usually your GitHub org/user name.
     projectName: "docs", // Usually your repo name.
+    trailingSlash: false,
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
